13b34a1928588451feed57c9eb1f451ae30aa907
Sergey <VeryBlackCatxD@gmail.com>
2019-03-07 16:10:12 +0200

    add products

--- bicycle-shop/src/containers/Gallery.js
+++ bicycle-shop/src/containers/Gallery.js
@@ -6,23 +6,42 @@ import Filter from '../containers/Filter.js';
 import Sort from './Sort.js';
 
 const Gallery = (props) => {
-    const config = (category) => {
+    const filtersConfig = (category) => {
+        const accessories = { filters: ['product', 'color'] };
+        const apparel = { filters: ['product', 'type', 'brand', 'color', 'size'] };
+        const backpacks = { filters: ['color', 'capacity'] };
         const bikes = { filters: ['type', 'brand', 'wheelSize', 'color'] };
+        const components = { filters: ['product', 'type', 'brand', 'color'] };
+        const news = { filters: ['product'] };
         const rental = { filters: ['type', 'brand', 'wheelSize', 'title'] };
+        const sale = { filters: ['product'] };
+        const tools = { filters: ['product', 'brand', 'color']};
         
         switch (category) {
             case 'bikes':
                 return bikes;
             case 'rental':
                 return rental;
+            case 'accessories':
+                return accessories;
+            case 'backpacks':
+                return backpacks;
+            case 'apparel':
+                return apparel;
             case 'components':
-                return bikes;
+                return components;
+            case 'news':
+                return news;
+            case 'sale':
+                return sale;
+            case 'tools':
+                return tools;    
             default:
                 return bikes;
         }
     }
     
-    const extraProps = config(props.match.params.category);//тут буду прокидывать все настройки для элементов галлереи   
+    const extraProps = filtersConfig(props.match.params.category);//тут буду прокидывать все настройки для элементов галлереи   
     
     return (
         <Container>
