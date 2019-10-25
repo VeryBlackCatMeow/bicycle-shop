bff45deda39697829e63eee4caaf9cc1807645ba
VeryBlackCat <veryblackcatxd@gmail.com>
2019-10-25 23:51:00 +0300

    add getDerivedStateFromProps to Filter

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -6,16 +6,17 @@ import Filter from '../containers/Filter';
 import Sort from '../containers/Sort';
 
 const Gallery = (props) => {
+
     const filtersConfig = (category) => {
-        const accessories = { filters: ['product', 'color'] };
-        const apparel = { filters: ['product', 'type', 'brand', 'color', 'size'] };
-        const backpacks = { filters: ['color', 'capacity'] };
-        const bicycles = { filters: ['type', 'brand', 'wheel Size', 'color'] };
-        const components = { filters: ['product', 'type', 'brand', 'color'] };
-        const news = { filters: ['product', 'brand'] };
-        const rental = { filters: ['type', 'brand', 'wheel Size', 'title'] };
-        const sale = { filters: ['product', 'brand'] };
-        const tools = { filters: ['product', 'brand', 'color']};
+        const accessories = ['product', 'color'];
+        const apparel = ['product', 'type', 'brand', 'color', 'size'];
+        const backpacks = ['color', 'capacity'];
+        const bicycles = ['type', 'brand', 'wheel Size', 'color'];
+        const components = ['product', 'type', 'brand', 'color'];
+        const news = ['product', 'brand'];
+        const rental = ['type', 'brand', 'wheel Size', 'title'];
+        const sale = ['product', 'brand'];
+        const tools = ['product', 'brand', 'color'];
         
         switch (category) {
             case 'bicycles':
@@ -41,13 +42,13 @@ const Gallery = (props) => {
         }
     }
     
-    const extraProps = filtersConfig(props.match.params.category);//тут буду прокидывать все настройки для элементов галлереи   
+    const filtersList = filtersConfig(props.match.params.category);//тут буду прокидывать все настройки для элементов галлереи   
     
     return (
         <Container className="gallery">
             <Row> 
                 <Col xs="3" lg="2">
-                    <Filter extraProps={extraProps}/> 
+                    <Filter filtersList={filtersList}/> 
                 </Col>
                 <Col xs="9" lg="10">
                     <h3 className="product-category">{props.match.params.category}</h3>
